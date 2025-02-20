75959efca635f53e561d8c26875b8bd14e76597d
induwra <induwaraisira793@gmail.com>
2025-02-20 18:03:00 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -169,7 +169,7 @@ cmd({
             if (!text) return reply("Hey bie please tell me for which pic you're looking");
             let name1 = text.split("|")[0]
             let name2 = text.split("|")[1] || `1`
-            citel.reply(`📸▶Sending ${name2} image(s) of ${name1} in chat\n📍ᴄʀᴇᴀᴛɪɴɢ ʙʏ ᴠᴀᴊɪʀᴀ📍`)
+            citel.reply(`📸▶Sending ${name2} image(s) of ${name1} in chat\n📍ᴄʀᴇᴀᴛɪɴɢ ʙʏ ｉｎｄｕｗａｒａ📍`)
             let nn = name2
             for (let i = 0; i < nn; i++) {
 
